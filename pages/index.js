389e6652eb0a36b5ede44327e5d7e8c7cfbd1470
Ezequias Santos <ezequiasmg16@gmail.com>
2020-12-31 09:12:38 -0300

    Para ospedar

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home (){
+    window.alert('Olá, Mundo!');
+    return document.getElementById('content').innerHTML='<object type="type/html" data="index.html"></object>';
+}
+export default Home
