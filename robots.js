1a70d9e83a447362f380ba20753a2b712fc216de
Ezequias Santos <ezequiasmg16@gmail.com>
2021-01-11 20:15:06 -0300

    Novo template
    começando um novo template esperimental para Algoritimando-oficial.

--- robots.js
+++ robots.js
@@ -1,1 +1,15 @@
-/*inicio*/
+<!-- jQuery -->
+<script type="text/javascript" src="../libs/jquery/jquery.js"></script>
+
+<!-- SmartMenus jQuery plugin -->
+<script type="text/javascript" src="../jquery.smartmenus.js"></script>
+
+<!-- SmartMenus jQuery init -->
+<script type="text/javascript">
+	$(function() {
+		$('#main-menu').smartmenus({
+			subMenusSubOffsetX: 1,
+			subMenusSubOffsetY: -8
+		});
+	});
+</script>
