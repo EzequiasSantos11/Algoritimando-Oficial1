3c8c2567d62048ea64bb3fbdc06f7b436956c8d3
Ezequias Santos <ezequiasmg16@gmail.com>
2021-02-11 18:54:04 -0300

    sessao conhecimentos
    melhorias na responsividade e criação da sessão conhecimentos

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,169 @@
 function Home (){
     window.alert('Olá, Mundo!');
-    return document.getElementById('content').innerHTML='<object type="type/html" data="index.html"></object>';
+    return
+    <html lang="pt-br">
+    
+    <head>
+        <script src="https://kit.fontawesome.com/aca70b3a45.js" crossorigin="anonymous"></script>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="/img/favicon/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/img/favicon/android-chrome-192x192.png" type="img/png" sizes="192x192">
+        <link rel="icon" href="/img/favicon/android-chrome-512x512.png" type="img/png" sizes="512x512">
+        <link rel="icon" href="/img/favicon/apple-touch-icon.png" type="img/png" sizes="180x180">
+        <link rel="icon" href="/img/favicon/favicon-16x16.png" type="img/png" sizes="16x16">
+        <link rel="icon" href="/img/favicon/favicon-32x32.png" type="img/png" sizes="32x32">
+        <link rel="manifest" href="/site.webmanifest">
+    
+        <title>Algoritimando</title>
+        <link rel="stylesheet" href="/style.css">
+    
+    </head>
+    
+    <body>
+        <div class="container">
+            <div id="cta-principal">
+                <div class="img-wrapper">
+                    <img src="/img/background-img-menor.jpg" alt="">
+                </div>
+                <div class="banner">
+                    <h1>Algoritimando a Biblionet dos DEV's</h1>
+                    <p>For: Ezequias Santos</p>
+                    <button>Saiba Mais</button>
+                </div>
+            </div>
+                <div class="header1">
+                    <nav class="container">
+                        <ul class="grid-12 header1">
+                            <li><a href="#" ><i class="fas fa-phone-square-alt" id="phone"></i>(38) 99896-5795</a></li>
+                            <li> <a href="#" ><i class="fab fa-youtube"  id="Youtube"></i> Mundo Geek</a></li>
+                            <li> <a href="#" ><i  class="fas fa-link" id="link"></i> Compartilhar </a></li>
+                            <li> <a href="#" > <i class="fas fa-dollar-sign" id="dinheiro"></i> Fazer uma doação</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <header>
+                    <div class="menu-resp menu-tela-m">
+                        <!--<img src="/img/Algoritimando cort.jpg" alt="Algoritimando">-->
+                        <H1>Algoritimando</H1>
+                        <nav id="nav-bar">
+                            <ul>
+                                <li><a href="#">Início</a></li>
+                                <li><a href="#">Docs.code</a></li>
+                                <li><a href="#">Projetos</a></li>
+                                <li><a href="#">Dev Games</a></li>
+                                <li><a href="#">Sobre Nós</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </header>
+                <div id="comp">
+                    <div class="fechar">
+                        <i class="fas fa-times" id="close-menu"></i>
+                        <span>Fechar Menu</span>
+                    </div>
+                    <div id="lines">
+                        <div class="line" id="line1"></div>
+                        <div class="line" id="line2"></div>
+                        <div class="line" id="line3"></div>
+                    </div>
+                </div>
+                <aside class="sidebar">
+                    <nav>
+                        <ul class="menu">
+                            <li class="menu-item"><a href="#" class="menu-link">Início</a></li>
+                            <li class="menu-item"><a href="#" class="menu-link">Docs.code</a></li>
+                            <li class="menu-item"><a href="#" class="menu-link">Projetos</a></li>
+                            <li class="menu-item"><a href="#" class="menu-link">Dev Games</a></li>
+                            <li class="menu-item"><a href="#" class="menu-link">Sobre nós</a></li>
+                        </ul>
+                    </nav>
+                    <div class="social-media">
+                        <a href="#"><i class="fab fa-facebook-square" id="facebook"></i></a>
+                        <a href="#"><i class="fab fa-github" id="github"></i></a>
+                        <a href="#"><i class="fab fa-instagram" id="instagram"></i></a>
+                    </div>
+                </aside>
+            <section class="sessao-conecimentos">
+                <div class="sessao-header">
+                    <h1>Aqui você encontra!</h1>
+                </div>
+                <div class="conhecimentos">
+                    <div class="conhecimento">
+                        <div class="conhecimento-header">
+                            <i class="fab fa-js-square"></i>
+                            <h3>JavaScript</h3>
+                            <div class="conecimento-text">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque odit sapiente
+                                    quo.Perferendis adipisci odio voluptas tenetur reprehenderit libero possimus odit cumque
+                                    similique, dolor provident ad sit amet laborum soluta.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="conhecimento">
+                        <div class="conhecimento-header">
+                            <i class="fab"></i>
+                            <h3>Criação de Sites</h3>
+                            <div class="conecimento-text">
+                                <p>Ducimus temporibus nihil, nobis eum cum facilis hic reprehenderit natus est eveniet quod
+                                    dolorem, maiores obcaecati cupiditate eius nam illo nulla voluptas incidunt amet. Alias
+                                    ex in quos qui consectetur.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="conhecimento">
+                        <div class="conhecimento-header">
+                            <i class="fab"></i>
+                            <h3>Manipulação de dados MSQL</h3>
+                            <div class="conecimento-text">
+                                <p>Illo a molestias, illum dolores praesentium reprehenderit facere! Itaque ratione ab
+                                    voluptate accusamus praesentium consequuntur? Temporibus, dolorem, illo qui obcaecati
+                                    culpa nobis ut, corrupti harum quaerat ducimus iusto hic laudantium!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="conhecimento">
+                        <div class="conhecimento-header">
+                            <i class="fab"></i>
+                            <h3>Criação de jogos</h3>
+                            <div class="conecimento-text">
+                                <p>Obcaecati deserunt numquam repellat doloribus facilis fugit tempora natus mollitia qui
+                                    saepe eos, atque animi ab error nulla voluptatem. Non repellendus inventore vero vitae
+                                    enim. Numquam dignissimos ullam rerum animi.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="conhecimento">
+                        <div class="conhecimento-header">
+                            <i class="fab"></i>
+                            <h3>Material de apoio</h3>
+                            <div class="conecimento-text">
+                                <p>Possimus earum, ab tenetur inventore nemo deserunt provident illo quos ad eveniet, sint
+                                    suscipit animi vel voluptatem praesentium incidunt. Doloribus iusto eius, placeat fugit
+                                    voluptate perferendis maiores officiis unde fuga.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="conhecimento">
+                        <div class="conhecimento-header">
+                            <i class="fab"></i>
+                            <h3>Video aulas completas</h3>
+                            <div class="conecimento-text">
+                                <p>Ad, aut voluptatibus omnis esse minima, incidunt consectetur veniam dolore atque possimus
+                                    nesciunt cumque facere magni? Molestias voluptatem delectus minima dolor vel veniam,
+                                    iure aut provident aliquid dolorem. Reiciendis, necessitatibus.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    
+    </body>
+    <script src="script.js">
+    
+    </script>
+    
+    </html>
 }
 export default Home
